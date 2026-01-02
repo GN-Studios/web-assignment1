0f9ed977b4cfde53bb9dc3e53636656726fd2dfc
Noam <noamnahman143@gmail.com>
2026-01-02 23:39:17 +0200

    added the post model and env vars

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import express, { Request, Response } from "express";
+import mongoose, { ConnectOptions } from "mongoose";
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(
+  `mongodb://${process.env.MONGODB_HOST || "localhost:27017"}/${
+    process.env.MONGODB_DB || "assignment-1"
+  }`,
+  {} as ConnectOptions
+);
+
+const DB = mongoose.connection;
+DB.on("error", (error) => {
+  console.error("connection error:", error);
+});
+DB.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
